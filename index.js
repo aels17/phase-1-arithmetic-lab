c44967541d8687afe3a344f37c56cdb296342858
Ayman <aelsayed.nh@gmail.com>
2022-01-10 13:11:27 -0500

    commit tree 68a168afa9bad58d5d85ed2f03ef33b0dbc1bd3a

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+   const num1 = 62;
+  const num2 = 1;
+  const num3 = 8;
+  const num4 = 2;
+function multiply (num1, num2)
+{
+
+ num1 * num2;}
+
+function random(){
+    Math.floor(Math.random() * 10);
+}
+
+function mod (num3, num4) {
+num3 /= num4;
+}
+
+function max() 
+{
+    Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,20);
+//=> 5
+}
